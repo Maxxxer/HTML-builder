4f45418c54a47d0587ebdfc57b65cf79d08d438c
Maxxxer <polonets.maksim@gmail.com>
2022-11-06 11:52:32 +0300

    feat: write makeDir function and callback

--- 04-copy-directory/index.js
+++ 04-copy-directory/index.js
@@ -1,1 +1,19 @@
+const { mkdir } = require('node:fs/promises');
+const path = require('path');
+const { copyFile, constants } = require('node:fs')
 
+async function makeDir(){
+  const projectFolder = path.join(__dirname, 'new-folder');
+  const createDir = await mkdir(projectFolder, { recursive: true });
+  return createDir;
+} 
+
+function callback(err) {
+  if (err) throw err;
+  console.log('hi')
+}
+
+
+
+
+makeDir().catch(console.error)
